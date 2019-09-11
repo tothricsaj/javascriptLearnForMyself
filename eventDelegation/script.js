9d960a4a3ab314f5ed7010e35574028b71351cc8
tothricsaj <tothricsaj@gmail.com>
2019-09-11 15:43:29 +0200

    added events

--- eventDelegation/script.js
+++ eventDelegation/script.js
@@ -1,1 +1,11 @@
 console.log('Hello Event Delegation!');
+
+let el = document.querySelector('.parent');
+
+el.addEventListener('click', function () {
+    console.log('%cClicked.....', 'background: orange; color: black; padding: 5px;');
+});
+
+el.addEventListener('mouseleave', function () {
+    console.log('%cYou leaved the listened area!', 'background: lightblue; color: white; paddint: 3px;')
+});
