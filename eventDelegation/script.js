6c7f76e7164b9877ff4b9268d3f743917c260ed8
tothricsaj <tothricsaj@gmail.com>
2019-09-11 15:48:40 +0200

    more child

--- eventDelegation/script.js
+++ eventDelegation/script.js
@@ -2,6 +2,7 @@ console.log('Hello Event Delegation!');
 
 let el = document.querySelector('.parent');
 
+// add evnet-listener to the parent
 el.addEventListener('click', function () {
     console.log('%cClicked.....', 'background: orange; color: black; padding: 5px;');
 });
