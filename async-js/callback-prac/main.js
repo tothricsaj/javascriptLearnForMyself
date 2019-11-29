0810b08ec317f6138c9fb6585092a4200e668db6
tothricsaj <tothricsaj@gmail.com>
2019-11-29 17:29:22 +0100

    :fire: learning

--- async-js/callback-prac/main.js
+++ async-js/callback-prac/main.js
@@ -1,1 +1,13 @@
-console.log('hello callback');;
+console.log('hello callback\n');
+
+function count(callback) {
+    console.log(1);
+    callback();
+    console.log(2);
+}
+
+function myCallback() {
+    console.log(3);
+}
+
+count(myCallback);
