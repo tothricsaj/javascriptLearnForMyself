22e3481b2a73401d44518ba9fad8d5e6979f1baa
tothricsaj <tothricsaj@gmail.com>
2019-12-02 11:34:57 +0100

    promise is folder is added/asyncGreeting :tulip:

---
+++ async-js/promise-prac/main.js
@@ -0,0 +1,9 @@
+console.log('hello promise!!!');
+
+function asyncGreeting() {
+    console.log('hello belloooooooo......what\'s the matter?');
+}
+
+function afterAsync() {
+    console.log('Well, finnally!');
+}
