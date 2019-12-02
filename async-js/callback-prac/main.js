65854df935aeccaf6cbfcc31b695988b930c175f
tothricsaj <tothricsaj@gmail.com>
2019-12-02 10:56:15 +0100

    :confused: intresting

--- async-js/callback-prac/main.js
+++ async-js/callback-prac/main.js
@@ -10,6 +10,13 @@ function myCallback() {
     setTimeout(() => {
         console.log(3);
     }, 3000);
+
+
+    setTimeout(() => {
+        console.log(4);
+    }, 4000);
+
 }
 
 count(myCallback);
+console.log(2.5);
