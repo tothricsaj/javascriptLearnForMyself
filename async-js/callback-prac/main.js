ec1c5440ee60fd32282e595e76cb5037b036d0f7
tothricsaj <tothricsaj@gmail.com>
2019-12-02 11:17:01 +0100

    image load with simulation

--- async-js/callback-prac/main.js
+++ async-js/callback-prac/main.js
@@ -20,3 +20,19 @@ function myCallback() {
 
 count(myCallback);
 console.log(2.5);
+
+function swapPic(elem) {
+    let img = `<img src="https://images.pexels.com/photos/2823459/pexels-photo-2823459.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="some pic"width=300 />`;
+    console.log(img);
+    setTimeout(() => {
+        elem.innerHTML = img;
+    }, 4000);
+}
+
+function loadElementsToWrapper(callback) {
+    let wrapper = document.querySelector('.wrapper');
+    callback(wrapper);
+}
+
+loadElementsToWrapper(swapPic);
+
