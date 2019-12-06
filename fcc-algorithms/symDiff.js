7ff5eddf496c7017df7ea85dbad576cb34401f98
tothricsaj <tothricsaj@gmail.com>
2019-12-06 15:59:10 +0100

    :poop: no working

--- fcc-algorithms/symDiff.js
+++ fcc-algorithms/symDiff.js
@@ -2,12 +2,27 @@ let arr1 = [1,2,3];
 let arr2 = [5, 2, 1, 4];
 
 function symDiff(...args) {
-  let mergedArr = [];
-  let result = [];
-  for(let i=0; i<args.length; i++) {
-    mergedArr = [...mergedArr, ...args[i]];
-  }
-  return mergedArr;
+    let mergedArr = [];
+    let result = [];
+
+    for(let i=0; i<args.length; i++) {
+        mergedArr = [...mergedArr, ...args[i]];
+    }
+    
+    for(let i=0; i<mergedArr.length; i++) {
+        if(!result.includes(mergedArr[i])) {
+            result.push(mergedArr[i]);
+        } else {
+            let index = mergedArr.indexOf(mergedArr[i]);
+            result.splice(index,1);
+        }
+    }
+
+    return result;
 }
 
 console.log(symDiff(arr1, arr2));
+console.log(symDiff([1, 2, 3, 3], [5, 2, 1, 4]));
+console.log(symDiff([1, 2, 5], [2, 3, 5], [3, 4, 5]));
+console.log(symDiff([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]));
+console.log(symDiff([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]));
