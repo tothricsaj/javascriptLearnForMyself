be4609706571fbdee881d62c24ec431d50d293a9
tothricsaj <tothricsaj@gmail.com>
2019-12-09 11:59:04 +0100

    separated checkMoreAndLess function

--- fcc-algorithms/symDiff.js
+++ fcc-algorithms/symDiff.js
@@ -6,36 +6,49 @@ const makeSetFromArr = (arr) => {
     return arr.filter((v, i) => arr.indexOf(v) === i).sort();
 }
 
+function checkMoreAndLess() {
+
+    if(this.arr1 > this.arr2) {
+        this.moreArr = this.arr1;
+        this.lessArr = this.arr2;
+    } else if(this.arr1 < this.arr2) {
+        this.moreArr = this.arr2;
+        this.lessArr = this.arr1;
+    } else {
+        this.moreArr = this.arr1;
+        this.lessArr = this.arr2;
+    }
+}
+
 function segment(arr1, arr2) {
     /*
      * make segment of two set
     */
 
-    let moreArr;
-    let lessArr;
+    this.arr1 = arr1;
+    this.arr2 = arr2;
+    
+    this.arr1 = makeSetFromArr(this.arr1);
+    this.arr2 = makeSetFromArr(this.arr2);
 
-    arr1 = makeSetFromArr(arr1);
-    arr2 = makeSetFromArr(arr2);
-
-    if(arr1 > arr2) {
-        moreArr = arr1;
-        lessArr = arr2;
-    } else if(arr1 < arr2) {
-        moreArr = arr2;
-        lessArr = arr1;
-    } else {
-        moreArr = arr1;
-        lessArr = arr2;
-    }
-
+    this.moreArr = '';
+    this.lessArr = '';
+    
+    checkMoreAndLess();
+    
     return moreArr.filter((value, index) => {
         return lessArr.indexOf(value) !== -1;
     });
 }
 
-// console.log(segment(arr1, arr2));
-// console.log(segment([1, 2, 5], [2, 3, 5]));
-// console.log(segment([6, 2, 1, 5, 7], [3, 4, 6, 6, 7]));
+console.log(segment(arr1, arr2));
+console.log(segment([1, 2, 5], [2, 3, 5]));
+console.log(segment([6, 2, 1, 5, 7], [3, 4, 6, 6, 7]));
+
+/*
+function symDiffOnlyTwoArr(arr1, arr2) {
+    
+}
 
 
 function symDiff(...args) {
@@ -55,7 +68,6 @@ function symDiff(...args) {
 
 console.log(symDiff(arr1, arr2));
 
-/*
 console.log(symDiff(arr1, arr2));
 console.log(symDiff([1, 2, 3, 3], [5, 2, 1, 4]));
 console.log(symDiff([1, 2, 5], [2, 3, 5], [3, 4, 5]));
