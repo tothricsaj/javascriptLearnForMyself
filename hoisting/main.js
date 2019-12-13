93ec9f105cfc2d3a502dc1c1c3cce4b28443fe98
tothricsaj <tothricsaj@gmail.com>
2019-12-13 14:08:17 +0100

    yheaa :apple:

--- hoisting/main.js
+++ hoisting/main.js
@@ -1,19 +1,19 @@
 console.log(sum(3,5));
+
+city = 'Szeged';
 console.log(city);
-console.log(name);
+// console.log(name);
+
 
 function sum (x, y) {
     return x + y;
 }
 
-const name = 'trz';
+// const name;
 let info = {
     age: 31,
     nationality: 'hungarian'
 };
 
-var city = 'Szeged';
 
-console.log(city);
-console.log(name);
-
+var city;
