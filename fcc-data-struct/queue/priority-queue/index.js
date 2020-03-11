db705ba7f33a8bb86310fb05acdc462b0cab821f
tothricsaj <tothricsaj@gmail.com>
2020-03-11 11:59:22 +0100

    I am must learn algorithms before :bus:

---
+++ fcc-data-struct/queue/priority-queue/index.js
@@ -0,0 +1,9 @@
+function PriorityQueue () {
+    this.collection = [];
+    this.printCollection = function() {
+      console.log(this.collection);
+    };
+    // Only change code below this line
+
+    // Only change code above this line
+}
