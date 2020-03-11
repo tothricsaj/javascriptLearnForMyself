5e2dd0f19b32a4b5b75a519785f9daa42fc70ab6
tothricsaj <tothricsaj@gmail.com>
2020-03-11 12:04:03 +0100

    sym function

--- fcc-algorithms/symDiff.js
+++ fcc-algorithms/symDiff.js
@@ -2,3 +2,7 @@ const diff = (arr1, arr2) => [
     ...arr1.filter(el => !arr2.includes(el)),
     ...arr2.filter(el => !arr1.includes(el)),
 ]
+
+const sym = (...args) => [...new Set(args.reduce(diff))]
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4]))
