13cc4398829760ecb6d292a192ce1dcafcd8f122
tothricsaj <tothricsaj@gmail.com>
2020-03-12 12:34:26 +0100

    completed task

--- fcc-algorithms/inventoryUpdate.js
+++ fcc-algorithms/inventoryUpdate.js
@@ -13,7 +13,11 @@ function updateInventory(arr1, arr2) {
         if(!tmpArr.includes(newEl[1])) arr1.push(newEl)
     });
 
-    return arr1
+    return arr1.sort((a, b) => {
+        if(a[1] < b[1]) return -1
+        else if(a[1] > b[1]) return 1
+        else return 0
+    })
 }
 
 // Example inventory lists
