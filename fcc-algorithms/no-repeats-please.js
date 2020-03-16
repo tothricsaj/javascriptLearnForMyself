3a6e2d2ac68313754d253b48e2a6840c5b7c47af
tothricsaj <tothricsaj@gmail.com>
2020-03-16 13:52:38 +0100

    duplicated permutation

--- fcc-algorithms/no-repeats-please.js
+++ fcc-algorithms/no-repeats-please.js
@@ -2,6 +2,8 @@ let swap = function (arr, index1, index2) {
     let tmp = arr[index1]
     arr[index1] = arr[index2]
     arr[index2] = tmp
+
+    return arr
 }
 
 let heapPermut = function(arr, output, n) {
@@ -11,9 +13,9 @@ let heapPermut = function(arr, output, n) {
         output(arr)
         console.log(arr)
     } else {
-        for(let i=1; i<n; i++) {
+        for(let i=1; i<=n; i++) {
 
-            heapPermut(n - 1, arr)
+            heapPermut(arr, output, n-1)
 
             if(n%2) {
                 swap(arr, 0, n-1)
