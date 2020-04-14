37ba14f7bc131b09ccbd3aafef1a1a60127d4001
tothricsaj <tothricsaj@gmail.com>
2020-04-14 21:24:01 +0200

    function with private and others staff

--- inharitance/Elf.js
+++ inharitance/Elf.js
@@ -1,9 +1,27 @@
 function Elf(name, weapon) {
     this.name = name
     this.weapon = weapon
+
+    let self = this
+    this.build = function() {
+        console.log(meditate())
+        return `${this.name} is building something!!!!`
+    }
+
+    function meditate() {
+        return `${self.name} is meditating!!!!`
+    }
+
+    console.log(this)
 }
 
 Elf.prototype.attack = function() {
     return `${this.name} attack with ${this.weapon}`
 }
 
+
+let peter = new Elf('Peter', 'sword')
+
+console.log(peter.attack())
+console.log(peter.build())
+console.log(Elf.prototype)
