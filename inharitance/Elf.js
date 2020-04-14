08a07ac4b8e56dc6ccb0a12706ece214bb15eb51
tothricsaj <tothricsaj@gmail.com>
2020-04-14 21:45:43 +0200

    DarkElf

--- inharitance/Elf.js
+++ inharitance/Elf.js
@@ -1,8 +1,10 @@
 function Elf(name, weapon) {
+
+    let self = this
+
     this.name = name
     this.weapon = weapon
 
-    let self = this
     this.build = function() {
         console.log(meditate())
         return `${this.name} is building something!!!!`
@@ -11,17 +13,30 @@ function Elf(name, weapon) {
     function meditate() {
         return `${self.name} is meditating!!!!`
     }
-
-    console.log(this)
 }
 
 Elf.prototype.attack = function() {
     return `${this.name} attack with ${this.weapon}`
 }
 
+function DarkElf(name, weapon, magic) {
+    Elf.call(this, name, weapon)
+    this.magic = magic
+}
+
+DarkElf.prototype = Object.create(Elf.prototype)
+
+DarkElf.prototype.makeMagic = function() {
+    return `${this.name} uses the ${this.magic} spell`
+}
+
 
 let peter = new Elf('Peter', 'sword')
+let darkPeter = new DarkElf('DarkPeter', 'wind', 'dragon eye')
 
 console.log(peter.attack())
 console.log(peter.build())
-console.log(Elf.prototype)
+
+console.log(darkPeter.attack())
+console.log(darkPeter.build())
+console.log(darkPeter.makeMagic())
