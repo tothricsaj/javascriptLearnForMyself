1ecbb6c60e434719d4b2e0ce248cd3048eeaafd5
tothricsaj <tothricsaj@gmail.com>
2020-04-16 20:59:45 +0200

    basic funcitons

---
+++ fp/Fight.js
@@ -0,0 +1,17 @@
+// attack
+// defense
+// heal
+// magic
+
+function Elf() {
+
+}
+
+function Wizzard() {
+
+}
+
+const attack = () => {}
+const defense = () => {}
+const heal = () => {}
+const magic = () => {}
