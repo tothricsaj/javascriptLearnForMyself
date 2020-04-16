60720495a4528639e9d5dd1c4babe7ec83734903
tothricsaj <tothricsaj@gmail.com>
2020-04-16 16:29:08 +0200

    global no have cache

--- fp/memozieation1.js
+++ fp/memozieation1.js
@@ -1,14 +1,19 @@
-let cache = {}
 
-function greeting(name, greeting) {
-    if(name in cache) {
-        return `${greeting} Good to see you ${cache[name]}`
-    } else {
-        console.error('new participant in the cache')
-        cache[name] = name
-        return `${greeting} Good to see you ${cache[name]}`
+function greetingClosure() {
+    let cache = {}
+
+    return function(name, greeting) {
+        if(name in cache) {
+            return `${greeting} Good to see you ${cache[name]}`
+        } else {
+            console.error('new participant in the cache')
+            cache[name] = name
+            return `${greeting} Good to see you ${cache[name]}`
+        }
     }
 }
+
+let greeting = greetingClosure()
 
 console.log(greeting('John', 'Welcome!'))
 console.log(greeting('John', 'Welcome!'))
