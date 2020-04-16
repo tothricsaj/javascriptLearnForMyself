d2625afbbf6f743aef9b26de74d93348d1d01868
tothricsaj <tothricsaj@gmail.com>
2020-04-16 15:51:28 +0200

    greeting example

--- fp/currying/currying.js
+++ fp/currying/currying.js
@@ -1,6 +1,14 @@
 const multiplyBy = (a) => (b) => a*b
-
 const multiplyBy5 = multiplyBy(5)
 
 console.log(multiplyBy5(2))
 console.log(multiplyBy5(10))
+
+const welcome = (name) => (greeting) => `Dear ${name}, ${greeting} Good to see you here!`
+const welcomeJosh = welcome('Josh')
+const welcomeGandalf = welcome('Gandalf')
+
+console.log(welcomeJosh('Good day!'))
+console.log(welcomeJosh('Welcome!'))
+
+console.log(welcomeGandalf('Greatings big wizzard!'))
