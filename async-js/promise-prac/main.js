6e1e9a578667e7a2231abca774017fad25660dce
tothricsaj <tothricsaj@gmail.com>
2020-04-17 11:40:59 +0200

    sourounding code

--- async-js/promise-prac/main.js
+++ async-js/promise-prac/main.js
@@ -2,6 +2,11 @@ let promise = new Promise((resolve, reject) => {
     setTimeout(() => resolve('Success!!!'), 1000)
 })
 
+console.log('1')
+
 promise.then((msg) => {
+    console.log('2')
     console.log('Hell yhea!! ' + msg)
 })
+
+console.log('3')
