c8e328e64eb922222216269c35f1c443b453011b
tothricsaj <tothricsaj@gmail.com>
2020-06-03 14:30:57 +0200

    push is impleneted :smile:2

--- DS/singliyLinkedList/main.js
+++ DS/singliyLinkedList/main.js
@@ -12,4 +12,17 @@ class SinglyLinkedList {
         this.next = null
         this.length = 0
     }
+
+    push(val) {
+        let newNode = new Node(val)
+        if(!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
 }
