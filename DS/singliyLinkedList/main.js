bd3522817ec8fa3c31ac4a85079bdce9580069d6
tothricsaj <tothricsaj@gmail.com>
2020-06-03 14:20:49 +0200

    SinglyLinkedList class

--- DS/singliyLinkedList/main.js
+++ DS/singliyLinkedList/main.js
@@ -4,3 +4,12 @@ class Node {
         this.next = null
     }
 }
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.next = null
+        this.length = 0
+    }
+}
