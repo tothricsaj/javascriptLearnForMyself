937b38880cc53299b2b8598846e322f970eb2043
tothricsaj <tothricsaj@gmail.com>
2020-06-15 15:32:36 +0200

    Push methode is implemented

--- DS/doublyLinkedList/main.js
+++ DS/doublyLinkedList/main.js
@@ -12,4 +12,23 @@ class DoublyLinkedList {
         this.tail = null
         this.length = 0
     }
+
+    push(val) {
+        let newNode = new Node(val)
+
+        if(this.head === null) this.head = newNode
+        else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+
+        this.length++
+        return this
+
+    }
 }
+
+let dll = new DoublyLinkedList()
+
+dll.push(3)
