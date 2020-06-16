45dbead5141dc38c2fc1e2922cac4351b02c8345
tothricsaj <tothricsaj@gmail.com>
2020-06-16 16:10:44 +0200

    pop method

--- DS/doublyLinkedList/main.js
+++ DS/doublyLinkedList/main.js
@@ -30,8 +30,30 @@ class DoublyLinkedList {
         return this
 
     }
+
+    pop() {
+        if(!this.head) return undefined
+        let poppedNode = this.tail
+        if(this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = poppedNode.prev
+            this.tail.next = null
+        }
+
+        this.length--
+        return poppedNode
+    }
 }
 
 let dll = new DoublyLinkedList()
 
 console.log(dll.push(3))
+
+dll.push(4)
+dll.push(5)
+
+console.log(dll)
+console.log(dll.pop().val)
+console.log(dll)
