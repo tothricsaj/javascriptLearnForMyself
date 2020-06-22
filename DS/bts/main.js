53fb7a4d7171578b44bb27a9d59e67124d72a5a0
tothricsaj <tothricsaj@gmail.com>
2020-06-22 15:10:38 +0200

    left side of tree

--- DS/bts/main.js
+++ DS/bts/main.js
@@ -10,4 +10,25 @@ class BST {
     constructor() {
         this.root = null
     }
+
+    insert(value) {
+        let newNode = new Node(value)
+
+        if(this.root === null) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root
+            while(true) {
+                if(value < current.value) {
+                    if(current.left === null) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                }
+            }
+        }
+    }
 }
