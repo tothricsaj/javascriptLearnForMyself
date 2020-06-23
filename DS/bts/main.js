1b771c72d322d56e9aa8eacdee9540cadae7e241
tothricsaj <tothricsaj@gmail.com>
2020-06-23 15:42:26 +0200

    contains method

--- DS/bts/main.js
+++ DS/bts/main.js
@@ -41,4 +41,22 @@ class BST {
             }
         }
     }
+
+    contains(value) {
+        if(this.root === null) return false
+        
+        let current = this.root,
+            found = false
+
+        while(current && !found) {
+            if(value < current.value) {
+                current = current.left
+            } else if(value > current.value) {
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false
+    }
 }
